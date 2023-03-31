4627c45d94aa2b03fca8e664725d5490c90e974f
Richard Trisnosaputra <r.trisnosaputra@teravintech.com>
2023-03-31 08:08:10 +0700

    adding create

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -3,11 +3,19 @@ import { getRepository, Repository } from 'typeorm'
 import Movie from '../entity/movie'
 
 const readAll = async(ctx: Koa.Context) => {
-    const movieRepo: Repository<Movie> = getRepository(Movie)
-    const movies = await movieRepo.find()
+    const repo: Repository<Movie> = getRepository(Movie)
+    const movies = await repo.find()
     ctx.body = movies
 }
 
+const create = async(ctx: Koa.Context) => {
+    const repo: Repository<Movie> = getRepository(Movie)
+    const movie: Movie = repo.create(ctx.request.body)
+    await repo.save(movie)
+    ctx.body = movie
+}
+
 export {
-    readAll
+    readAll,
+    create
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import * as Router from 'koa-router'
-import { readAll } from '../controller/index'
+import { readAll, create } from '../controller/index'
 
 const router: Router = new Router()
 
 router.get('/', readAll)
+router.post('/', create)
 
 export default router
